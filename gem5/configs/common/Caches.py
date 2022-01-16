2932e28a9558b8d33dfa3cc1072559b6d1d5c5b3
RyoTTa <jhyn207@gmail.com>
2022-01-16 08:53:22 -0500

    For MIX generate configure

--- gem5/configs/common/Caches.py
+++ gem5/configs/common/Caches.py
@@ -70,8 +70,8 @@ class L2Cache(Cache):
     tag_latency = 20
     data_latency = 20
     response_latency = 20
-    fill_latency = 60
-    write_latency = 60
+    fill_latency = 20
+    write_latency = 20
     mshrs = 20
     tgts_per_mshr = 12
     bank_number = 1
