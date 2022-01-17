55920d7bc1ef81f809b65bdad8d352a890178b21
RyoTTa <jhyn207@gmail.com>
2022-01-17 04:03:34 -0500

    All Mix benchmark workload configuration

--- GEM5_Script/makeMIX.py
+++ GEM5_Script/makeMIX.py
@@ -4,22 +4,21 @@ from itertools import combinations
 benchName = {
     'mcf' : 'inp.in',
     'lbm' : '3000 reference.dat 0 0 100_100_130_ldc.of',
-    'soplex' : '-s1 -e -m45000 pds-50.mps',
     'milc' : '<su3imp.in',
     'sphinx3' : './ctlfile . args.an4',
     'GemsFDTD' : '',
     'leslie3d' : '<leslie3d.in',
-
     'cactusADM' : 'benchADM.par',
     'bzip2' : 'chicken.jpg 30',
-    #'h264ref' : '-d foreman_ref_encoder_baseline.cfg',
-    #'astar' : 'rivers.cfg',
+    'h264ref' : '-d foreman_ref_encoder_baseline.cfg',
+    'astar' : 'rivers.cfg',
+    'libquantum' : '1397 8',
     #'povray' : 'SPEC-benchmark-ref.ini'
-
     #'gcc' : '166.i -o 166.s',
 
+    #'soplex' : '-s1 -e -m45000 pds-50.mps',
     #'omnetpp' : 'omnetpp.ini',
-    #'libquantum' : '1397 8',
+
     #'bwaves' : 'bwaves.in',
     #'zeusmp' : '',
     #'xalancbmk' : '-v t5.xml xalanc.xsl',
@@ -29,7 +28,8 @@ benchName_keys = list(benchName.keys())
 
 bench = list(combinations(benchName_keys, 4))
 
-print(len(bench))
+
+
 
 '''
 for name1 in benchName_keys :
@@ -45,3 +45,6 @@ for name1 in benchName_keys :
 for temp in range(0,len(bench)) :
     print('mix;' + bench[temp][0] + ';' + bench[temp][1] + ';' + bench[temp][2] + ';' + bench[temp][3] + ' : ',end = '')
     print(benchName[bench[temp][0]] + ';' + benchName[bench[temp][1]] + ';' +benchName[bench[temp][2]] + ';' +benchName[bench[temp][3]])
+
+
+print(len(bench))
